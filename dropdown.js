fc57b7b1d70ed2107954b47a99d3665b59032819
Tanmay Arun Navghare <tanmay.navghare@gmail.com>
2016-04-12 18:24:39 +0530

    Create dropdown.js

---
+++ dropdown.js
@@ -0,0 +1,9 @@
+(function($){
+
+// Iterate over each select element
+    /*$('select').each(function () {
+        $(this).selectmenu();
+    });*/
+    $("#selectbox1").selectmenu();
+})(jQuery);
+
